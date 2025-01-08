08ba9e0f14ae5692259960a15623a5a708c66836
Gianpaolo Alvari <40580633+galvari@users.noreply.github.com>
2025-01-08 12:25:53 +0100

    Update run_compute_coordination.sh

--- run_compute_coordination.sh
+++ run_compute_coordination.sh
@@ -1,11 +1,3 @@
-# python src/estimate_coordination.py OUTPUT/JSON_copycodec/1348_high_copycodec_coords.json --input_video OUTPUT/Render_copycodec/1348_high_copycodec_gaze.mp4 --output_folder OUTPUT/Render_copycodec/
-# python src/estimate_coordination.py OUTPUT/JSON_copycodec/1472_med_copycodec_coords.json --input_video OUTPUT/Render_copycodec/1472_med_copycodec_gaze.mp4 --output_folder OUTPUT/Render_copycodec/
-# python src/estimate_coordination.py OUTPUT/JSON_copycodec/1507_med_copycodec_coords.json --input_video OUTPUT/Render_copycodec/1507_med_copycodec_gaze.mp4 --output_folder OUTPUT/Render_copycodec/
-# python src/estimate_coordination.py OUTPUT/JSON_copycodec/1541_high_copycodec_coords.json --input_video OUTPUT/Render_copycodec/1541_high_copycodec_gaze.mp4 --output_folder OUTPUT/Render_copycodec/
-# python src/estimate_coordination.py OUTPUT/JSON_copycodec/1670_low_copycodec_coords.json --input_video OUTPUT/Render_copycodec/1670_low_copycodec_gaze.mp4 --output_folder OUTPUT/Render_copycodec/
-# python src/estimate_coordination.py OUTPUT/JSON_copycodec/1730_high_copycodec_coords.json --input_video OUTPUT/Render_copycodec/1730_high_copycodec_gaze.mp4 --output_folder OUTPUT/Render_copycodec/
-# python src/estimate_coordination.py OUTPUT/JSON_copycodec/1815_med_copycodec_coords.json --input_video OUTPUT/Render_copycodec/1815_med_copycodec_gaze.mp4 --output_folder OUTPUT/Render_copycodec/
-# python src/estimate_coordination.py OUTPUT/JSON_copycodec/982_low_copycodec_coords.json --input_video OUTPUT/Render_copycodec/982_low_copycodec_gaze.mp4 --output_folder OUTPUT/Render_copycodec/
 
 INPUT_JSON_FOLDER=$1
 COORDINATION_FACTOR=$2
