53000844176fd8ef42f8c5cdd78041a5efbd5ee6
galvari <alvari.gianpaolo@gmail.com>
2025-01-20 16:24:40 +0100

    Added new scripts and data files

---
+++ optional_run_visualize_faces.sh
@@ -0,0 +1,11 @@
+python src/visualize_faces.py ../INPUT/JSON/Example_nocopycodec/1348_high/ ../INPUT/Videos/Example_mp4/1348_high.mp4 ../OUTPUT/Render_nocopycodec/
+python src/visualize_faces.py ../INPUT/JSON/Example_nocopycodec/1472_med/ ../INPUT/Videos/Example_mp4/1472_med.mp4 ../OUTPUT/Render_nocopycodec/
+python src/visualize_faces.py ../INPUT/JSON/Example_nocopycodec/1507_med/ ../INPUT/Videos/Example_mp4/1507_med.mp4 ../OUTPUT/Render_nocopycodec/
+python src/visualize_faces.py ../INPUT/JSON/Example_nocopycodec/1541_high/ ../INPUT/Videos/Example_mp4/1541_high.mp4 ../OUTPUT/Render_nocopycodec/
+python src/visualize_faces.py ../INPUT/JSON/Example_nocopycodec/1670_low/ ../INPUT/Videos/Example_mp4/1670_low.mp4 ../OUTPUT/Render_nocopycodec/
+python src/visualize_faces.py ../INPUT/JSON/Example_nocopycodec/1730_high/ ../INPUT/Videos/Example_mp4/1730_high.mp4 ../OUTPUT/Render_nocopycodec/
+python src/visualize_faces.py ../INPUT/JSON/Example_nocopycodec/1815_med/ ../INPUT/Videos/Example_mp4/1815_med.mp4 ../OUTPUT/Render_nocopycodec/
+python src/visualize_faces.py ../INPUT/JSON/Example_nocopycodec/1830_high/ ../INPUT/Videos/Example_mp4/1830_high.mp4 ../OUTPUT/Render_nocopycodec/
+python src/visualize_faces.py ../INPUT/JSON/Example_nocopycodec/982_low/ ../INPUT/Videos/Example_mp4/982_low.mp4 ../OUTPUT/Render_nocopycodec/
+python src/visualize_faces.py /home/galvari/EYEC/INPUT/JSON/Demo/EYEC_VideoDemo/ /home/galvari/EYEC/INPUT/Videos/Demo/EYEC_VideoDemo.mp4/ /home/galvari/EYEC/OUTPUT/Demo
+
